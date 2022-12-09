65e1be809bdcbc81fbac2cf3ebdd45eca6e6d060
Tiger Taylor <tigertaylor1024@gmail.com>
2022-12-09 09:49:55 +0000

    Return JWT when logging in and registering

--- api/auth.py
+++ api/auth.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from random import randint
 from flask import Blueprint, request
+from jwtlib import JWT
 from client import get_client
 from cipher import Hasher, Encryptor
 
@@ -7,6 +10,7 @@ auth_bp = Blueprint("auth_bp", __name__)
 client = get_client()
 hasher = Hasher()
 encryptor = Encryptor()
+jwt = JWT()
 
 
 @auth_bp.route("/login", methods=["POST"])
@@ -20,21 +24,25 @@ def login():
     email_hash = hasher.sha256_hash(email)
 
     # Get the user from the database.
-    doc = client.collection("users").document(email_hash).get()
-
-    try:
-        user = doc.to_dict()
-        password_hash = user["password"]
-    except ValueError:
+    email_doc = client.collection("emails").document(email_hash)
+    email_json = email_doc.get().to_dict()
+    if email_doc.get().exists:
+        user_doc = client.collection("users").document(email_json["user_id"]).get()
+        password_hash = user_doc.to_dict()["password"]
+    else:
         password_hash = ""
 
     valid_password = hasher.argon_verify(password_hash, password)
 
     # Check if the user exists and the password is valid.
-    if not (doc.exists and valid_password):
+    if not valid_password:
         return {"error": "Username or password is invalid"}, 401
 
-    return {"token": "fake-token"}, 200
+    client.collection("sessions").document(
+        session_id := str(randint(0, 2**32 - 1))
+    ).set({"user_id": email_json["user_id"]})
+
+    return {"token": jwt.create_token(email_json["user_id"], session_id)}, 200
 
 
 @auth_bp.route("/register", methods=["POST"])
@@ -52,22 +60,34 @@ def register():
     if not (email and name and password):
         return {"error": "Email, name and password are required"}, 400
 
+    # User information
+    user_id = hasher.sha256_hash(email + str(datetime.now()))
     email_hash = hasher.sha256_hash(email)
     password_hash = hasher.argon_hash(password)
     encrypted_name = encryptor.encrypt(name)
     encrypted_email = encryptor.encrypt(email)
 
+    # Get user_id from emails collection and get the corresponding user
+    email_doc = client.collection("emails").document(email_hash)
+
     # Check email is not already registered
-    doc = client.collection("users").document(email_hash).get()
-    if doc.exists:
+    if email_doc.get().exists:
         return {"error": "Email already exists"}, 400
 
     # Add user to users collection
-    client.collection("users").document(email_hash).set(
+    email_doc.set({"user_id": user_id})
+    user_doc = client.collection("users").document(email_doc.get().to_dict()["user_id"])
+    user_doc.set(
         {
             "email": encrypted_email,
             "name": encrypted_name,
             "password": password_hash,
         }
     )
-    return {"token": "fake-token"}, 201
+
+    # Create a new session
+    client.collection("sessions").document(
+        session_id := str(randint(0, 2**32 - 1))
+    ).set({"user_id": user_id})
+
+    return {"token": jwt.create_token(user_id, session_id)}, 201
