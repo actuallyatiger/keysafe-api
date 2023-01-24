5c9e9ea51f59acf91a60bbdae55f860e6a4112f1
Tiger Taylor <tigertaylor1024@gmail.com>
2023-01-24 11:09:14 +0000

    Added /creds/createCredential

--- api/creds.py
+++ api/creds.py
@@ -1,12 +1,15 @@
-from flask import Blueprint
-from middleware import refresh_jwt
+from cipher import Encryptor
 from client import get_client
+from flask import Blueprint, request
 from jwtlib import JWT
+from middleware import refresh_jwt
 
 client = get_client()
 jwt = JWT()
 
 cred_bp = Blueprint("cred_bp", __name__)
+
+encryptor = Encryptor()
 
 
 @cred_bp.route("/getCredentials", methods=["GET"])
@@ -60,3 +63,57 @@ def get_credential(token, cred_id: str):
         200,
         {"Content-Type": "application/json"},
     )
+
+
+@cred_bp.route("/createCredential", methods=["POST"])
+@refresh_jwt
+def create_credential(token):
+    """
+    Create a new credential.
+    """
+    contents = jwt.decode_token(token)
+
+    cred = client.collection("credentials").document()
+
+    cred.set(
+        {
+            "user_id": contents["user_id"],
+            "name": "",
+            "email": "",
+            "password": "",
+            "url": "",
+        }
+    )
+
+    return {"token": token, "id": cred.id}, 200, {"Content-Type": "application/json"}
+
+
+@cred_bp.route("/setCredential/<string:cred_id>", methods=["POST"])
+@refresh_jwt
+def update_credential(token, cred_id: str):
+    """
+    Update a credential.
+    """
+    contents = jwt.decode_token(token)
+
+    cred = client.collection("credentials").document(cred_id)
+    cred_got = cred.get()
+
+    if not cred_got.exists:
+        return {"error": "Credential not found"}, 404
+
+    cred_dict = cred_got.to_dict()
+
+    if cred_dict["user_id"] != contents["user_id"]:
+        return {"error": "Unauthorized"}, 401
+
+    cred_sanitized = {}
+    for key in cred_dict:
+        if key in {"name", "url"}:
+            cred_sanitized[key] = request.json[key]
+        elif key in {"email", "password"}:
+            cred_sanitized[key] = encryptor.encrypt(request.json[key])
+
+    cred.update(cred_sanitized)
+
+    return {"token": token}, 200, {"Content-Type": "application/json"}
